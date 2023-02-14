f429ee8e47aa853304bc8e65fe7bc69635a658b0
delet-dis <efimov.036@gmail.com>
2023-02-14 20:50:04 +0700

    🗂️ added basic project structure

---
+++ TimeFlow/TimeFlow/TimeFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFlowApp.swift
+//  TimeFlow
+//
+//  Created by Igor Efimov on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
