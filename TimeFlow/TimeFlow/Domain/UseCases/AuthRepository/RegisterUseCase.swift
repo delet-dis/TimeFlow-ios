9e1518b74a491133d506ac2888985b07df9fdc64
sema_alimpiev <jonerty04@gmail.com>
2023-02-27 19:34:46 +0700

    🔧 add authorization useCase

---
+++ TimeFlow/TimeFlow/Domain/UseCases/AuthRepository/RegisterUseCase.swift
@@ -0,0 +1,33 @@
+//
+//  RegisterUseCase.swift
+//  TimeFlow
+//
+//  Created by Семён Алимпиев on 26.02.2023.
+//
+
+import Foundation
+import NeedleFoundation
+
+protocol RegisterUseCaseDependency: Dependency {
+    var registerUseCaseProvider: RegisterUseCase { get }
+}
+
+class RegisterUseCase {
+    private let authRepository: AuthRepository
+
+    init(authRepository: AuthRepository) {
+        self.authRepository = authRepository
+    }
+
+    func executeStudent(request: StudentRegisterCredentials, completion: ((Result<VoidResponse, Error>) -> Void)? = nil) {
+        authRepository.studentRegistration(studentRegisterCredentials: request, completion: completion)
+    }
+
+    func executeTeacher(request: TeacherCredentials, completion: ((Result<VoidResponse, Error>) -> Void)? = nil) {
+        authRepository.teacherRegistration(teacherRegisterCredentials: request, completion: completion)
+    }
+
+    func executeExternalUser(request: ExternalUserCredentials, completion: ((Result<VoidResponse, Error>) -> Void)? = nil) {
+        authRepository.externalUserRegistration(externalUserRegisterCredentials: request, completion: completion)
+    }
+}
