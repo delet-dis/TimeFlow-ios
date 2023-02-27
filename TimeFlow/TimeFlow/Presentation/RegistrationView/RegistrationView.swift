732723682e3d89890ab8383f6f48097418e29dab
delet-dis <efimov.036@gmail.com>
2023-02-27 21:03:28 +0700

    👁️ scroll indicators hidden

--- TimeFlow/TimeFlow/Presentation/RegistrationView/RegistrationView.swift
+++ TimeFlow/TimeFlow/Presentation/RegistrationView/RegistrationView.swift
@@ -46,7 +46,7 @@ struct RegistrationView: View {
             }
             .padding(.horizontal, 10)
 
-            ScrollView {
+            ScrollView(showsIndicators: false) {
                 VStack(spacing: 10) {
                     SharedRegistrationFormView(
                         viewData: $viewModel.sharedRegistrationData,
