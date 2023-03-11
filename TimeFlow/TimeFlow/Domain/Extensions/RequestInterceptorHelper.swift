9d6836db5b98ee4ab8c6f016861b04832ec07070
sema_alimpiev <jonerty04@gmail.com>
2023-03-11 17:01:32 +0700

    🔑 edit change refreshToken

--- TimeFlow/TimeFlow/Domain/Extensions/RequestInterceptorHelper.swift
+++ TimeFlow/TimeFlow/Domain/Extensions/RequestInterceptorHelper.swift
@@ -12,14 +12,20 @@ import SwiftUI
 class RequestInterceptorHelper: RequestInterceptor {
     private var saveTokenUseCase: SaveTokensUseCase
     private var refreshTokenUseCase: RefreshTokenUseCase
+    private var logoutUseCase: LogoutUseCase
+    private var getTokenUseCase: GetTokenUseCase
     private var newToken = ""
 
     init(
         saveTokenUseCase: SaveTokensUseCase,
-        refreshTokenUseCase: RefreshTokenUseCase
+        refreshTokenUseCase: RefreshTokenUseCase,
+        logoutUseCase: LogoutUseCase,
+        getTokenUseCase: GetTokenUseCase
     ) {
         self.saveTokenUseCase = saveTokenUseCase
         self.refreshTokenUseCase = refreshTokenUseCase
+        self.logoutUseCase = logoutUseCase
+        self.getTokenUseCase = getTokenUseCase
     }
 
     func adapt(
@@ -44,28 +50,35 @@ class RequestInterceptorHelper: RequestInterceptor {
         dueTo error: Error,
         completion: @escaping (RetryResult) -> Void
     ) {
-        if let response = request.task?.response as? HTTPURLResponse,
-           response.statusCode == NetworkingConstants.wrongDataStatusCode {
-            refreshTokenUseCase.execute { [weak self] result in
-                switch result {
-                case .success(let result):
-                    self?.newToken = result.accessToken
-
-                    self?.saveTokenUseCase.execute(
-                        authToken: result.accessToken,
-                        refreshToken: result.refreshToken
-                    ) { [weak self] result in
+        getTokenUseCase.execute { [self] result in
+            switch result {
+            case .success(let token):
+                if let response = request.task?.response as? HTTPURLResponse,
+                   response.statusCode == NetworkingConstants.wrongAccessToken {
+                    refreshTokenUseCase.execute { [weak self] result in
                         switch result {
-                        case .success:
-                            completion(.retry)
-
+                        case .success(let result):
+                            self?.newToken = result.accessToken
+                            self?.saveTokenUseCase.execute(
+                                authToken: result.accessToken,
+                                refreshToken: result.refreshToken
+                            ) { [weak self] result in
+                                switch result {
+                                case .success:
+                                    completion(.retry)
+                                case .failure(let error):
+                                    print(error)
+                                }
+                            }
                         case .failure(let error):
+                            //TODO: LogOut
                             print(error)
                         }
                     }
-                case .failure(let error):
-                    print(error)
                 }
+            case .failure(let error):
+                // TODO: LogOut
+                print(error)
             }
         }
     }
